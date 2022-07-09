bcbfc648a8ec805c96c1d8ff7f0b62c12cd5ea92
Andrew <andrew.salomatinn@gmail.com>
2022-07-09 09:01:26 +0300

    Условия и циклы в теории. Создал отдельный файл для практических задач

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+// Работаем в новом режиме (строгий режим)
+"use strict";
+
 /*
 
 // Переменные
 
-// Работаем в новом режиме (строгий режим)
-"use strict";
 
 let number = 5;
 const leftBorderWidth = 1;
@@ -50,24 +51,97 @@ console.log(arr[0]);
 
 */
 
-// Практика
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false
-};
+// !!! Условия !!!
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+/*
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if () {
+   
+} else if {
+   
+} else {
 
-console.log(personalMovieDB);
+}
+
+const num = 50;
+
+if (num < 49) {
+   console.log('Error');
+} else if (num > 100) {
+   console.log('Too much');
+} else {
+   console.log("Okey");
+}
+
+// Если условие верно => ----- Если не верно =>
+(num === 50) ? console.log("Okey") : console.log('Error');
+
+
+switch (num) {
+   case 49:
+      console.log("To less");
+      break;
+   
+   case 50:
+      console.log("Ok");
+      break;
+
+   case 100:
+      console.log("To much")
+      break;
+
+   default:
+      console.log("Не судьба");
+      break;
+}
+
+*/
+
+// Циклы
+
+let num = 50;
+
+while (num <= 55) {
+   console.log(num);
+   num++;
+}
+
+do {
+   console.log(num);
+   num++;
+}
+while (num <= 55);
+
+for (let i = 1; i < 8; i++) {
+   console.log(num);
+   num++;
+}
+
+// break and continue работают тоже
+
+// Нужно сделать отдельный файл для практики и для теории
+
+// Вложенные циклы
+
+for (let i = 0; i < 3; i++) {
+   console.log(`i=${i}`);
+   for (let j = 0; j < 3; j++){
+      console.log(j);
+   }
+}
+
+
+let result = '';
+const lenght = 7;
+
+
+for (let i = 1; i < lenght; i++){
+   for (let j = 1; j < i; j++) {
+      result += '* ';
+   }
+   result += '\n';
+}
+
+console.log(result);
